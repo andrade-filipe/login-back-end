672bde7cba01645ace610b4ec0576c6b45bbc344
Filipe Andrade <101667610+andrade-filipe@users.noreply.github.com>
2023-11-01 12:56:25 -0300

    Organized code and added Test Classes

---
+++ src/test/java/com/project/login/domain/repositorys/UserRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.project.login.domain.repositorys;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void findByUsername() {
+    }
+
+    @Test
+    void findByUsernameOrEmail() {
+    }
+}
